d4f0fff045c1b937bfa248dcdab6c4b9f1a5220e
Ansonsd5 <anson11sd@gmail.com>
2024-08-21 22:51:35 +0530

    initial setup force

--- safeplay/vite.config.js
+++ safeplay/vite.config.js
@@ -2,7 +2,7 @@ import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
  plugins: [react()],
  resolve: {
@@ -10,10 +10,5 @@ export default defineConfig({
      "@": path.resolve(__dirname, "./src"),
    },
  },
- build: {
-  outDir: "dist",
-  rollupOptions: {
-    input: path.resolve(__dirname, 'index.html'), // Ensure the entry point is explicitly defined
-  },
-},
 })
+
