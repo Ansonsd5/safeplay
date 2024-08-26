bff1ee38bf1ec55066bdec07b89a1920191f70ae
Jayasurya <jayasurya@digital.ai>
2024-08-26 12:41:27 +0530

    move uv-description down

--- safeplay/src/components/features/Features.jsx
+++ safeplay/src/components/features/Features.jsx
@@ -27,14 +27,22 @@ function Features() {
         // area: true,
         // stack: 'total'
     };
-    const uvLevels = [
-        {riskLevel: 'low', description: 'Low (0-2)'},
-        {riskLevel: 'moderate', description: 'Moderate (3-5)'},
-        {riskLevel: 'high', description: 'High (6-7)'},
-        {riskLevel: 'very-high', description: 'Very High (8-10)'},
-    ];
+    // const uvLevels = [
+    //     {riskLevel: 'low', description: 'Low (0-2)'},
+    //     {riskLevel: 'moderate', description: 'Moderate (3-5)'},
+    //     {riskLevel: 'high', description: 'High (6-7)'},
+    //     {riskLevel: 'very-high', description: 'Very High (8-10)'},
+    // ];
     const [selectedCities, setSelectedCities] = useState([]);
     const [citiesData, setCitiesData] = useState([melbourneCityData]);
+
+    // // Function to determine UV risk level
+    // function getUVRiskLevel(uvIndex) {
+    //     if (uvIndex <= 2) return 'Low';
+    //     if (uvIndex <= 5) return 'Moderate';
+    //     if (uvIndex <= 7) return 'High';
+    //     return 'Very High';
+    // }
 
     function handleCityChange(event) {
         const {target: {value}} = event;
@@ -54,68 +62,73 @@ function Features() {
             <section className='features'>
                 <h2>Epic 1 Features</h2>
                 <div className='uv-radiation'>
-                    <div className='uv-left-section'>
-                        <p className='feature-name'>Compare UV Exposure</p>
-                        <LineChart
-                            xAxis={[
-                                {
-                                    id: 'seasons',
-                                    data: seasonLabels,
-                                    label: 'Seasons',
-                                    scaleType: 'point',
-                                },
-                            ]}
-                            yAxis={[
-                                {
-                                    id: 'uv-exposure',
-                                    label: 'UV Index',
-                                },
-                            ]}
-                            series={citiesData}
-                            width={500}
-                            height={300}
-                            margin={{top: 50}}
-                        />
-                    </div>
-                    <div className='uv-right-section'>
-                        <Box className='cityInput' sx={{minWidth: 150}}>
-                            <FormControl fullWidth>
-                                <Select labelId='city-select-label'
-                                        id='city-select'
-                                        value={selectedCities}
-                                        onChange={handleCityChange}
-                                        multiple
-                                        displayEmpty
-                                        renderValue={selected =>
-                                            selected.length === 0 ? 'Select a city...' : selected.join(', ')
-                                        }
-                                >
-                                    {cities.map(city => (
-                                        <MenuItem key={city} value={city}>
-                                            <Checkbox checked={selectedCities.indexOf(city) > -1}/>
-                                            <ListItemText primary={city}/>
-                                        </MenuItem>
-                                    ))}
-                                </Select>
-                            </FormControl>
-                        </Box>
-                        <div className='uv-info'>
-                            <p className='feature-name'>UV Index</p>
-                            <div className='uv-level-info'>
-                                {uvLevels.map((level, index) => (
-                                    <div key={index} className='uv-level'>
-                                        <span className={`uv-pill ${level.riskLevel}`}></span>
-                                        {level.description}
-                                    </div>
-                                ))}
-                            </div>
-                            <p className='uv-description'>
-                                The UV index is a measure of the strength of the sun's ultraviolet (UV) rays. It is a scale
-                                primarily used in daily forecasts aimed at the general public. The UV index is designed as an
-                                open-ended linear scale, directly proportional to the intensity of UV radiation that causes
-                                sunburn on human skin.
-                            </p>
+                    <div className='uv-chart-city'>
+                        <div className='uv-left-section'>
+                            <p className='feature-name'>Compare UV Exposure</p>
+                            <LineChart
+                                xAxis={[
+                                    {
+                                        id: 'seasons',
+                                        data: seasonLabels,
+                                        label: 'Seasons',
+                                        scaleType: 'point',
+                                    },
+                                ]}
+                                yAxis={[
+                                    {
+                                        id: 'uv-exposure',
+                                        label: 'UV Index',
+                                    },
+                                ]}
+                                series={citiesData}
+                                width={500}
+                                height={300}
+                                margin={{top: 50}}
+                            />
                         </div>
+                        <div className='uv-right-section'>
+                            <Box className='cityInput' sx={{minWidth: 150}}>
+                                <FormControl fullWidth>
+                                    <Select labelId='city-select-label'
+                                            id='city-select'
+                                            value={selectedCities}
+                                            onChange={handleCityChange}
+                                            multiple
+                                            displayEmpty
+                                            renderValue={selected =>
+                                                selected.length === 0 ? 'Select a city...' : selected.join(', ')
+                                            }
+                                    >
+                                        {cities.map(city => (
+                                            <MenuItem key={city} value={city}>
+                                                <Checkbox checked={selectedCities.indexOf(city) > -1}/>
+                                                <ListItemText primary={city}/>
+                                            </MenuItem>
+                                        ))}
+                                    </Select>
+                                </FormControl>
+                            </Box>
+                            {/*<div className='uv-info'>*/}
+                            {/*    <p className='feature-name'>UV Index</p>*/}
+                            {/*    <div className='uv-level-info'>*/}
+                            {/*        {uvLevels.map((level, index) => (*/}
+                            {/*            <div key={index} className='uv-level'>*/}
+                            {/*                <span className={`uv-pill ${level.riskLevel}`}></span>*/}
+                            {/*                {level.description}*/}
+                            {/*            </div>*/}
+                            {/*        ))}*/}
+                            {/*    </div>*/}
+                            {/*</div>*/}
+                        </div>
+                    </div>
+                    <p className='uv-description'>
+                        The UV index is a measure of the strength of the sun's ultraviolet (UV) rays. It is a scale
+                        primarily used in daily forecasts aimed at the general public. The UV index is designed as an
+                        open-ended linear scale, directly proportional to the intensity of UV radiation that causes
+                        sunburn on human skin.
+                    </p>
+                    <div className='scroll-down'>
+                        <p>↓↓ Scroll down to check UV levels and precautions ↓↓</p>
                     </div>
                 </div>
                 <div className='uv-precautions'>
@@ -127,7 +140,7 @@ function Features() {
                 <h2>Epic 2 Features</h2>
                 <div className='allergic-rhinitis'>
                     <h2>Awareness of Allergic Rhinitis (Hay fever)</h2>
-                    <AllergicAwareness />
+                    <AllergicAwareness/>
                 </div>
                 <div className='prevent-hay-fever'>
                     <h2>Common Hay fever Triggers & Prevention</h2>
